eba087ce080c6e8acb0faeeca35b9796d66e0aba
also <wuzhyy@163.com>
2017-08-26 15:39:16 +0800

    merge php doc generator to init-php.el

--- lisp/init-web-mode.el
+++ lisp/init-web-mode.el
@@ -2,14 +2,13 @@
 
 (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tmpl\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.module\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.inc\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.xml?\\'" . web-mode))
 
 (require-package 'emmet-mode)
+(emmet-mode 1)
 (require-package 'php-auto-yasnippets)
 
 (setq web-mode-ac-sources-alist
@@ -17,11 +16,12 @@
         ("html" . (ac-source-emmet-html-aliases ac-source-emmet-html-snippets))
         ("css" . (ac-source-css-property ac-source-emmet-css-snippets))))
 
+(setq-default indent-tabs-mode nil)
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
-  ;;;(setq web-mode-code-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
   )
 
 (add-hook 'web-mode-hook  'my-web-mode-hook)
