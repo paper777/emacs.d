b290a0e9b5a0a446fdcf28b2e65ab18cadfe4af3
also <wuzhy@wrdtech.com>
2017-09-15 02:37:19 -0400

    remove crontab-mode

--- init.el
+++ init.el
@@ -83,7 +83,7 @@
 (require 'init-projectile)
 
 (require 'init-compile)
-;;(require 'init-crontab)
+;;;(require 'init-crontab)
 (require 'init-textile)
 (require 'init-markdown)
 (require 'init-csv)
